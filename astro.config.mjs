aea576558bfb1bcfd6e478a0995bba45ea77ef36
Andre Luiz M. de Abreu <andre.abreu@coop.caal.com.br>
2025-01-09 16:43:40 -0300

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,76 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import AutoImport from "astro-auto-import";
+import { defineConfig, squooshImageService } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import config from "./src/config/config.json";
+import languagesJSON from "./src/config/language.json";
+const { default_language } = config.settings;
+
+const supportedLang = [...languagesJSON.map((lang) => lang.languageCode)];
+const disabledLanguages = config.settings.disable_languages;
+
+// Filter out disabled languages from supportedLang
+const filteredSupportedLang = supportedLang.filter(
+  (lang) => !disabledLanguages.includes(lang),
+);
+
+console.log("Supported Languages:", filteredSupportedLang);
+console.log("Default Language:", default_language);
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  base: config.site.base_path ? config.site.base_path : "/",
+  trailingSlash: config.site.trailing_slash ? "always" : "ignore",
+  i18n: {
+    locales: filteredSupportedLang,
+    defaultLocale: default_language,
+  },
+  image: {
+    service: squooshImageService(),
+  },
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    AutoImport({
+      imports: [
+        "@/shortcodes/Button",
+        "@/shortcodes/Accordion",
+        "@/shortcodes/Notice",
+        "@/shortcodes/Video",
+        "@/shortcodes/Youtube",
+        "@/shortcodes/Tabs",
+        "@/shortcodes/Tab",
+      ],
+    }),
+    mdx(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+});
